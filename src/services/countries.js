e87d45d3039fee7c7366b95167b6533d0d893ce3
Kevin Do <kevind0501@gmail.com>
2021-12-07 15:29:00 -0800

    removed console.logs

--- src/services/countries.js
+++ src/services/countries.js
@@ -3,6 +3,5 @@ import { checkError, client } from './client';
 export async function getCountries() {
   // call supabase and get the list of restaurants
   const response = await client.from('countries').select();
-  console.log(response);
   return checkError(response);
 }
